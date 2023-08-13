cee80168321c59ae6edd82820926c0bf05f548ab
gopoma <gordono@unsa.edu.pe>
2023-08-12 23:53:18 -0500

    add IEEExtreme module

---
+++ IEEExtreme/archive/GCD.cpp
@@ -0,0 +1,70 @@
+#pragma GCC optimize(3,"Ofast","inline")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+
+#define MACRO(code) do {code} while (false)
+#define DBG(x) MACRO(cerr << #x << " = " << (x) << endl;)
+#define DBGY(x) MACRO(cerr << #x << " = " << (x) << " , ";)
+#define DBG2(x,y) MACRO(DBGY(x); DBG(y);)
+#define DBG3(x,y,z) MACRO(DBGY(x); DBGY(y); DBG(z);)
+#define DBG4(x,y,z,w) MACRO(DBGY(x); DBGY(y); DBGY(z); DBG(w);)
+#define RAYA MACRO(cerr << " ================ " << endl;)
+
+#define tcT template<class T
+
+using ll = long long;
+using ld = long double;       // or double if tight TL
+using u128 = __uint128_t;
+using str = string;         // yay python!
+tcT> using V = vector<T>;
+tcT, size_t SZ> using AR = array<T, SZ>;
+
+#define mp  make_pair
+#define f   first
+#define s   second
+
+#define sz(x)   int((x).size())
+#define bg(x)   begin(x)
+#define all(x)  bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x)  sort(all(x))
+#define rsz     resize
+#define ins     insert
+#define pb      push_back
+#define eb      emplace_back
+#define ft      front()
+#define bk      back()
+
+#define lb lower_bound
+#define ub upper_bound
+
+const int MOD = (int)1e9 + 7;   // 998244353;
+const ld PI = acos((ld)-1);
+mt19937 rng(0); // or mt19937_64
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};  // for every grid problem!!
+const char n_l = '\n';
+
+void solve() {
+    ll A, B;
+    cin >> A >> B;
+
+    cout << (__gcd(A, B)) << n_l;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll t = 1LL;
+    // cin >> t;
+
+    while(t--)
+        solve();
+
+    return 0;
+}
