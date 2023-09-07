1c933618b271a0020d19e87b84be5386f5f25f70
gopoma <gordono@unsa.edu.pe>
2023-09-07 00:44:10 -0500

    MinIEEEXtreme/08-NumberTheory

---
+++ MinIEEEXtreme/08-NumberTheory/G_BasicEuler-Mobius.cpp
@@ -0,0 +1,251 @@
+#pragma GCC optimize(3,"Ofast","inline")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+
+#define MACRO(code) do {code} while (false)
+#define DBG(x) MACRO(cerr << #x << " = " << (x) << endl;)
+#define DBGY(x) MACRO(cerr << #x << " = " << (x) << " , ";)
+#define DBG2(x,y) MACRO(DBGY(x); DBG(y);)
+#define DBG3(x,y,z) MACRO(DBGY(x); DBGY(y); DBG(z);)
+#define DBG4(x,y,z,w) MACRO(DBGY(x); DBGY(y); DBGY(z); DBG(w);)
+#define RAYA MACRO(cerr << " ================ " << endl;)
+
+#define tcT template<class T
+
+using ll = long long;
+using ld = long double;       // or double if tight TL
+using u64 = uint64_t;
+using u128 = __uint128_t;
+using str = string;         // yay python!
+tcT> using V = vector<T>;
+tcT, size_t SZ> using AR = array<T, SZ>;
+
+#define mp  make_pair
+#define F   first
+#define S   second
+
+#define sz(x)   int((x).size())
+#define bg(x)   begin(x)
+#define all(x)  bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x)  sort(all(x))
+#define rsz     resize
+#define ins     insert
+#define pb      push_back
+#define eb      emplace_back
+#define ft      front()
+#define bk      back()
+
+#define lb lower_bound
+#define ub upper_bound
+
+const int MOD = (int)1e9 + 7;   // 998244353;
+const ld PI = acos((ld)-1);
+mt19937 rng(0); // or mt19937_64
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};  // for every grid problem!!
+const char n_l = '\n';
+
+template <typename T>
+ostream& operator <<(ostream &os, const vector<T>& v) {
+    os << "[";
+
+    for(int i = 0; i < sz(v); i++) {
+        if (i > 0) os << " ";
+        os << v[i];
+    }
+    return os << "]";
+}
+
+long long phi(long long n) {
+    long long result = n;
+    for (long long i = 2LL; i * i <= n; i++) {
+        if (n % i == 0LL) {
+            while (n % i == 0LL)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1LL)
+        result -= result / n;
+    return result;
+}
+
+
+bool isSquareFree(long long n) {
+    if (n % 2LL == 0LL)
+        n = n/2LL;
+
+    if (n % 2LL == 0LL)
+        return false;
+
+    for (int i = 3LL; i <= sqrt(n); i = i+2LL) {
+        if (n % i == 0LL) {
+            n = n/i;
+
+            if (n % i == 0LL)
+               return false;
+        }
+    }
+
+    return true;
+}
+
+struct Mint {
+  uint64_t n;
+  static uint64_t mod, inv, r2;
+  Mint() : n(0) { }
+  Mint(const uint64_t &x) : n(init(x)) { }
+  static uint64_t init(const uint64_t &w) { return reduce(__uint128_t(w) * r2); }
+  static void set_mod(const uint64_t &m) {
+    mod = inv = m;
+    for(int i = 0; i < 5; i++)  inv *= 2 - inv * m;
+    r2 = -__uint128_t(m) % m;
+  }
+  static uint64_t reduce(const __uint128_t &x) {
+    uint64_t y = uint64_t(x >> 64) - uint64_t((__uint128_t(uint64_t(x) * inv) * mod) >> 64);
+    return int64_t(y) < 0 ? y + mod : y;
+  }
+  Mint& operator+= (const Mint &x) { n += x.n - mod; if(int64_t(n) < 0) n += mod; return *this; }
+  Mint& operator+ (const Mint &x) const { return Mint(*this) += x; }
+  Mint& operator*= (const Mint &x) { n = reduce(__uint128_t(n) * x.n); return *this; }
+  Mint& operator* (const Mint &x) const { return Mint(*this) *= x; }
+  uint64_t val() const { return reduce(n); }
+};
+
+uint64_t Mint::mod, Mint::inv, Mint::r2;
+
+bool suspect(const uint64_t &a, const uint64_t &s, uint64_t d, const uint64_t &n) {
+  if(Mint::mod != n)  Mint::set_mod(n);
+  Mint x(1), xx(a), one(x), minusone(n - 1);
+  while(d > 0) {
+    if(d & 1) x *= xx;
+    xx *= xx;
+    d >>= 1;
+  }
+  if(x.n == one.n)  return true;
+  for(unsigned int r = 0; r < s; r++) {
+    if(x.n == minusone.n) return true;
+    x *= x;
+  }
+  return false;
+}
+
+bool is_prime(const uint64_t &n) {
+  if(n <= 1 || (n > 2 && (~n & 1))) return false;
+  uint64_t d = n - 1, s = 0;
+  while(~d & 1) s++, d >>= 1;
+  static const uint64_t a_big[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+  static const uint64_t a_smol[] = {2, 7, 61};
+  if(n <= 4759123141LL) {
+    for(auto &&p : a_smol) {
+      if(p >= n)  break;
+      if(!suspect(p, s, d, n))  return false;
+    }
+  } else {
+    for(auto &&p : a_big) {
+      if(p >= n)  break;
+      if(!suspect(p, s, d, n))  return false;
+    }
+  }
+  return true;
+}
+
+uint64_t rho_pollard(const uint64_t &n) {
+  if(~n & 1)  return 2u;
+  static random_device rng;
+  uniform_int_distribution<uint64_t> rr(1, n - 1);
+  if(Mint::mod != n)  Mint::set_mod(n);
+  for(;;) {
+    uint64_t c_ = rr(rng), g = 1, r = 1, m = 500;
+    Mint y(rr(rng)), xx(0), c(c_), ys(0), q(1);
+    while(g == 1) {
+      xx.n = y.n;
+      for(unsigned int i = 1; i <= r; i++)  y *= y, y += c;
+      uint64_t k = 0; g = 1;
+      while(k < r && g == 1) {
+        for(unsigned int i = 1; i <= (m > (r - k) ? (r - k) : m); i++) {
+          ys.n = y.n;
+          y *= y; y += c;
+          uint64_t xxx = xx.val(), yyy = y.val();
+          q *= Mint(xxx > yyy ? xxx - yyy : yyy - xxx);
+        }
+        g = __gcd<uint64_t>(q.val(), n);
+        k += m;
+      }
+      r *= 2;
+    }
+    if(g == n)  g = 1;
+    while(g == 1) {
+      ys *= ys; ys += c;
+      uint64_t xxx = xx.val(), yyy = ys.val();
+      g = __gcd<uint64_t>(xxx > yyy ? xxx - yyy : yyy - xxx, n);
+    }
+    if(g != n && is_prime(g)) return g;
+  }
+  assert(69 == 420);
+}
+
+template <typename T>
+vector<T> factor(T n) {
+  if(n < 2) return { };
+  if(is_prime(n)) return {n};
+  T d = rho_pollard(static_cast<uint64_t>(n));
+  vector<T> l = factor(d), r = factor(n / d);
+  l.insert(l.end(), r.begin(), r.end());
+  return l;
+}
+
+
+long long mobius(long long n) {
+    if(!isSquareFree(n)) {
+        return 0LL;
+    } else {
+        if(isSquareFree(n)) {
+            vector<uint64_t> fac = factor(uint64_t(n));
+
+            unordered_set<uint64_t> SS;
+            for(auto& e: fac)
+                SS.ins(e);
+
+            long long count = 0LL;
+            for(auto& e: SS)
+                count += is_prime(e);
+
+            if(count & 1LL) {
+                return -1LL;
+            }
+        }
+
+        return 1LL;
+    }
+}
+
+void solve() {
+    long long n;
+    cin >> n;
+
+    assert(n >= 2LL);
+
+    long long ans = phi(n);
+    long long ans2 = mobius(n);
+
+    cout << ans << " " << ans2 << n_l;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll t = 1LL;
+    // cin >> t;
+
+    while(t--)
+        solve();
+
+    return 0;
+}
---
+++ MinIEEEXtreme/08-NumberTheory/L_ChineseRemainderTheorem.cpp
@@ -0,0 +1,120 @@
+#pragma GCC optimize(3,"Ofast","inline")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+
+#define MACRO(code) do {code} while (false)
+#define DBG(x) MACRO(cerr << #x << " = " << (x) << endl;)
+#define DBGY(x) MACRO(cerr << #x << " = " << (x) << " , ";)
+#define DBG2(x,y) MACRO(DBGY(x); DBG(y);)
+#define DBG3(x,y,z) MACRO(DBGY(x); DBGY(y); DBG(z);)
+#define DBG4(x,y,z,w) MACRO(DBGY(x); DBGY(y); DBGY(z); DBG(w);)
+#define RAYA MACRO(cerr << " ================ " << endl;)
+
+#define tcT template<class T
+
+using ll = long long;
+using ld = long double;       // or double if tight TL
+using u64 = uint64_t;
+using u128 = __uint128_t;
+using str = string;         // yay python!
+tcT> using V = vector<T>;
+tcT, size_t SZ> using AR = array<T, SZ>;
+
+#define mp  make_pair
+#define F   first
+#define S   second
+
+#define sz(x)   int((x).size())
+#define bg(x)   begin(x)
+#define all(x)  bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x)  sort(all(x))
+#define rsz     resize
+#define ins     insert
+#define pb      push_back
+#define eb      emplace_back
+#define ft      front()
+#define bk      back()
+
+#define lb lower_bound
+#define ub upper_bound
+
+const int MOD = (int)1e9 + 7;   // 998244353;
+const ld PI = acos((ld)-1);
+mt19937 rng(0); // or mt19937_64
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};  // for every grid problem!!
+const char n_l = '\n';
+
+template <typename T>
+ostream& operator <<(ostream &os, const vector<T>& v) {
+    os << "[";
+
+    for(int i = 0; i < sz(v); i++) {
+        if (i > 0) os << " ";
+        os << v[i];
+    }
+    return os << "]";
+}
+
+using T = __int128;
+// ax + by = __gcd(a, b)
+// returns __gcd(a, b)
+T extended_euclid(T a, T b, T &x, T &y) {
+  T xx = y = 0;
+  T yy = x = 1;
+  while (b) {
+    T q = a / b;
+    T t = b; b = a % b; a = t;
+    t = xx; xx = x - q * xx; x = t;
+    t = yy; yy = y - q * yy; y = t;
+  }
+  return a;
+}
+// finds x such that x % m1 = a1, x % m2 = a2. m1 and m2 may not be coprime
+// here, x is unique modulo m = lcm(m1, m2). returns (x, m). on failure, m = -1.
+pair<T, T> CRT(T a1, T m1, T a2, T m2) {
+  T p, q;
+  T g = extended_euclid(m1, m2, p, q);
+  if (a1 % g != a2 % g) return make_pair(0, -1);
+  T m = m1 / g * m2;
+  p = (p % m + m) % m;
+  q = (q % m + m) % m;
+  return make_pair((p * a2 % m * (m1 / g) % m + q * a1 % m * (m2 / g) % m) %  m, m);
+}
+
+template <typename T>
+inline T gcd(T a, T b) { while (b != 0) swap(b, a %= b); return a; }
+
+template <typename T>
+inline T lcm(T a, T b) { return (a * b) / gcd<T>(a, b); }
+
+void solve() {
+    int a, n, b, m;
+    cin >> a >> n >> b >> m;
+
+    pair<T, T> ans = CRT(a, n, b, m);
+
+    long long x = (long long)ans.first;
+    long long K = (long long)ans.second; // lcm<long long>((long long)n, (long long)m);
+
+    if(K == -1LL) cout << "-1\n";
+    else cout << x << " " << K << n_l;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll t = 1LL;
+    cin >> t;
+
+    while(t--)
+        solve();
+
+    return 0;
+}
