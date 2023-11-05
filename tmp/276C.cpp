fe7d45196b13b2ea511f0b94626e1dd47d0b4cf1
gopoma <gordono@unsa.edu.pe>
2023-11-05 00:41:53 -0500

    CF

---
+++ tmp/276C.cpp
@@ -0,0 +1,299 @@
+// sometimes pragmas don't work, if so, just comment it!
+#pragma GCC optimize(3,"Ofast","inline")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+// building blocks
+using ll = long long;
+using ull = unsigned long long;
+using db = long double; // or double, if TL is tight
+using str = string; // yay python!
+// using u128 = __uint128_t; // for Number Theory related
+// using i128 = __int128;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>; // minima
+
+
+
+// pairs
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+
+
+#define tcT template<class T
+#define tcTU tcT, class U
+// ^ lol this makes everything look weird but I'll try it
+
+tcT> using V = vector<T>;
+tcT, size_t SZ> using AR = array<T,SZ>;
+using vi = V<int>;
+using vb = V<bool>;
+using vl = V<ll>;
+using vd = V<db>;
+using vs = V<str>;
+using vpi = V<pi>;
+using vpl = V<pl>;
+using vpd = V<pd>;
+
+
+
+// using u128 = __uint128_t;
+tcT> using V = vector<T>;
+tcT, size_t SZ> using AR = array<T, SZ>;
+
+
+
+// vectors
+// oops size(x), rbegin(x), rend(x) need C++17
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+
+#define lb lower_bound
+#define ub upper_bound
+tcT> int lwb(V<T>& a, const T& b) { return int(lb(all(a),b)-bg(a)); }
+tcT> int upb(V<T>& a, const T& b) { return int(ub(all(a),b)-bg(a)); }
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define rep(a) F0R(_,a)
+#define each(a,x) for (auto& a: x)
+
+
+
+// bitwise ops
+// also see https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { // assert(x >= 0); // make C++11 compatible until USACO updates ...
+	return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x))
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); } // divide a by b rounded down
+
+
+
+#define tcTUU tcT, class ...U
+
+inline namespace FileIO {
+	void setIn(str s)  { freopen(s.c_str(),"r",stdin); }
+	void setOut(str s) { freopen(s.c_str(),"w",stdout); }
+	void setIO(str s = "") {
+		cin.tie(0)->sync_with_stdio(0); // unsync C / C++ I/O streams
+		// cin.exceptions(cin.failbit);
+		// throws exception when do smth illegal
+		// ex. try to read letter into int
+		if (sz(s)) setIn(s+".in"), setOut(s+".out"); // for old USACO
+	}
+}
+
+
+
+// for debugging!
+#define MACRO(code) do {code} while (false)
+#define DBG(x) MACRO(cerr << #x << " = " << (x) << endl;)
+#define DBGY(x) MACRO(cerr << #x << " = " << (x) << " , ";)
+#define DBG2(x,y) MACRO(DBGY(x); DBG(y);)
+#define DBG3(x,y,z) MACRO(DBGY(x); DBGY(y); DBG(z);)
+#define DBG4(x,y,z,w) MACRO(DBGY(x); DBGY(y); DBGY(z); DBG(w);)
+#define RAYA MACRO(cerr << " ================ " << endl;)
+
+template <typename T>
+ostream& operator <<(ostream &os, const vector<T>& v) {
+    os << "[";
+
+    for(int i = 0; i < int(v.size()); i++) {
+        if (i > 0) os << " ";
+        os << v[i];
+    }
+    return os << "]";
+}
+
+template <typename T>
+ostream& operator <<(ostream &os, const set<T>& v) {
+    vector<T> tmp;
+    for(auto& e: v) {
+        tmp.emplace_back(e);
+    }
+
+    return os << tmp;
+}
+
+template <typename T>
+ostream& operator <<(ostream &os, const pair<T, T>& v) {
+    return os << "{" << v.first << ", " << v.second << "}";
+}
+
+
+
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+// Direction vectors
+int dRow[] = { -1, 0, 1,  0 };
+int dCol[] = {  0, 1, 0, -1 };
+
+const int MOD = (int)1e9+7; // 998244353;
+const int MX = (int)2e5+5;
+const ll BIG = 1e18; // not too close to LLONG_MAX
+const db PI = acos((db)-1);
+const char n_l = '\n';
+
+template <typename T>
+inline T gcd(T a, T b) { while (b != 0) swap(b, a %= b); return a; }
+
+template<typename T>
+T pot(T a, T b) { // a^b
+    assert(b >= 0);
+
+    T res = 1;
+    for(int _ = 1; _ <= b; _++) res *= a;
+    return res;
+}
+
+// here goes the template!
+
+/**
+ * Description: 1D range increment and sum query.
+ * Source: USACO Counting Haybales
+ 	* https://codeforces.com/blog/entry/82400
+ * Verification: USACO Counting Haybales
+ */
+
+struct F { // lazy update
+	ll inc = 0;
+	F() {}
+	F(int x) { inc = x; }
+	F& operator*=(const F& a) { inc += a.inc; return *this; }
+}; V<F> lazy;
+
+struct T { // data you need to store for each interval
+	ll sz = 1, sum = 0;
+	T() {}
+	T(int x) { sum = x; }
+	friend T operator+(const T& a, const T& b) {
+		T res; res.sz = a.sz+b.sz;
+		res.sum = a.sum+b.sum;
+		return res;
+	}
+	T& operator*=(const F& a) {
+		sum += (ll)sz*a.inc; return *this; }
+}; V<T> seg;
+
+struct LazySeg {
+	int SZ = 1;
+	void init(const V<T>& _seg) {
+		while (SZ < sz(_seg)) SZ *= 2;
+		seg.rsz(2*SZ); lazy.rsz(2*SZ);
+		F0R(i,SZ) seg[SZ+i] = _seg[i];
+		ROF(i,1,SZ) pull(i);
+	}
+	void push(int ind) { /// modify values for current node
+		seg[ind] *= lazy[ind];
+		if (ind < SZ) F0R(i,2) lazy[2*ind+i] *= lazy[ind];
+		lazy[ind] = F();
+	} // recalc values for current node
+	void pull(int ind) { seg[ind] = seg[2*ind]+seg[2*ind+1]; }
+	void upd(int lo, int hi, F inc, int ind, int L, int R) {
+		push(ind); if (hi < L || R < lo) return;
+		if (lo <= L && R <= hi) {
+			lazy[ind] = inc; push(ind); return; }
+		int M = (L+R)/2; upd(lo,hi,inc,2*ind,L,M);
+		upd(lo,hi,inc,2*ind+1,M+1,R); pull(ind);
+	}
+	void upd(int lo, int hi, int inc) { upd(lo,hi,{inc},1,0,SZ-1); }
+	T query(int lo, int hi, int ind, int L, int R) {
+		push(ind); if (lo > R || L > hi) return T();
+		if (lo <= L && R <= hi) return seg[ind];
+		int M = (L+R)/2;
+		return query(lo,hi,2*ind,L,M)+query(lo,hi,2*ind+1,M+1,R);
+	}
+	T query(int lo, int hi) { return query(lo,hi,1,0,SZ-1); }
+};
+
+// /here goes the template!
+
+// here goes the work!
+LazySeg tree;
+void solve() {
+    int n, q;
+    cin >> n >> q;
+
+    V<ll> a(n);
+    for(auto& e: a) cin >> e;
+
+    sort(rall(a));
+
+
+    V<T> aux;
+    for(int _ = 0; _ < n; _++) aux.emplace_back(T());
+    tree.init(aux);
+
+
+    for(int _ = 0; _ < q; _++) {
+        int l, r;
+        cin >> l >> r;
+        l--; r--;
+
+        tree.upd(l, r, 1);
+    }
+
+    V<ll> cnt(n);
+    for(int i = 0; i < n; i++) {
+        cnt[i] = tree.query(i, i).sum;
+    }
+
+    sort(rall(cnt));
+
+    ll ans = 0;
+    for(int i = 0; i < n; i++) {
+        ans += a[i] * cnt[i];
+    }
+    cout << ans << n_l;
+}
+
+signed main() {
+    // read read read
+	setIO();
+    // cout << fixed << setprecision(12);
+
+    long long t = 1LL;
+    // cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    // should actually read the stuff at the bottom
+}
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+
+//! ERASE DBGs xd or TLE
