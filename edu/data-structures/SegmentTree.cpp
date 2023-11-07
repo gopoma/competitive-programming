97572c5dbb17ee4b2896c66446fa629802aabad2
gopoma <gordono@unsa.edu.pe>
2023-11-06 20:45:53 -0500

    CF

---
+++ edu/data-structures/SegmentTree.cpp
@@ -0,0 +1,315 @@
+// sometimes pragmas don't work, if so, just comment it!
+#pragma GCC optimize(3,"Ofast","inline")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+// building blocks
+using ll = long long;
+using ull = unsigned long long;
+using db = long double; // or double, if TL is tight
+using str = string; // yay python!
+// using u128 = __uint128_t; // for Number Theory related
+// using i128 = __int128;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>; // minima
+
+
+
+// pairs
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+
+
+#define tcT template<class T
+#define tcTU tcT, class U
+// ^ lol this makes everything look weird but I'll try it
+
+
+
+tcT> using V = vector<T>; //?
+tcT, size_t SZ> using AR = array<T,SZ>;
+using vi = V<int>;
+using vb = V<bool>;
+using vl = V<ll>;
+using vd = V<db>;
+using vs = V<str>;
+using vpi = V<pi>;
+using vpl = V<pl>;
+using vpd = V<pd>;
+
+// vectors
+// oops size(x), rbegin(x), rend(x) need C++17
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+#define ts to_string
+
+#define lb lower_bound
+#define ub upper_bound
+tcT> int lwb(V<T>& a, const T& b) { return int(lb(all(a),b)-bg(a)); }
+tcT> int upb(V<T>& a, const T& b) { return int(ub(all(a),b)-bg(a)); }
+
+
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define rep(a) F0R(_,a)
+#define each(a,x) for (auto& a: x)
+
+
+
+// bitwise ops
+// also see https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { // assert(x >= 0); // make C++11 compatible until USACO updates ...
+	return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x))
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+
+
+ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); } // divide a by b rounded down
+
+
+
+#define tcTUU tcT, class ...U
+
+inline namespace FileIO {
+	void setIn(str s)  { freopen(s.c_str(),"r",stdin); }
+	void setOut(str s) { freopen(s.c_str(),"w",stdout); }
+	void setIO(str s = "") {
+		cin.tie(0)->sync_with_stdio(0); // unsync C / C++ I/O streams
+		// cin.exceptions(cin.failbit);
+		// throws exception when do smth illegal
+		// ex. try to read letter into int
+		if (sz(s)) setIn(s+".in"), setOut(s+".out"); // for old USACO
+	}
+}
+
+
+
+// for debugging!
+#define MACRO(code) do {code} while (false)
+#define DBG(x) MACRO(cerr << #x << " = " << (x) << endl;)
+#define DBGY(x) MACRO(cerr << #x << " = " << (x) << " , ";)
+#define DBG2(x,y) MACRO(DBGY(x); DBG(y);)
+#define DBG3(x,y,z) MACRO(DBGY(x); DBGY(y); DBG(z);)
+#define DBG4(x,y,z,w) MACRO(DBGY(x); DBGY(y); DBGY(z); DBG(w);)
+#define RAYA MACRO(cerr << " ================ " << endl;)
+
+template <typename T>
+ostream& operator <<(ostream &os, const vector<T>& v) {
+    os << "[";
+
+    for(int i = 0; i < int(v.size()); i++) {
+        if (i > 0) os << " ";
+        os << v[i];
+    }
+    return os << "]";
+}
+
+template <typename T>
+ostream& operator <<(ostream &os, const set<T>& v) {
+    vector<T> tmp;
+    for(auto& e: v) {
+        tmp.emplace_back(e);
+    }
+
+    return os << tmp;
+}
+
+template <typename T>
+ostream& operator <<(ostream &os, const pair<T, T>& v) {
+    return os << "{" << v.first << ", " << v.second << "}";
+}
+
+
+
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+// Direction vectors
+int dRow[] = { -1, 0, 1,  0 };
+int dCol[] = {  0, 1, 0, -1 };
+
+const int MOD = (int)1e9+7; // 998244353;
+const int MX = (int)2e5+5;
+const ll BIG = 1e18; // not too close to LLONG_MAX
+const db PI = acos((db)-1);
+const char n_l = '\n';
+
+template <typename T>
+inline T gcd(T a, T b) { while (b != 0) swap(b, a %= b); return a; }
+
+template<typename T>
+T pot(T a, T b) { // a^b
+    assert(b >= 0);
+
+    T res = 1;
+    for(int _ = 1; _ <= b; _++) res *= a;
+    return res;
+}
+
+// here goes the template!
+vector<long long> get_prefix_sums(vector<long long>& a) {
+    const int n = int(a.size());
+
+    vector<long long> pref(n);
+    pref[0] = a[0];
+
+    for(int i = 1; i < n; i++) {
+        pref[i] = pref[i - 1] + a[i];
+    }
+    return pref;
+}
+
+long long query(vector<long long>& pref, int l, int r) {
+    long long res = pref[r];
+    if(l - 1 >= 0) res -= pref[l - 1];
+    return res;
+}
+
+/**
+ * Description: 1D point update and range query where \texttt{cmb} is
+ 	* any associative operation. \texttt{seg[1]==query(0,N-1)}.
+ * Time: O(\log N)
+ * Source:
+	* http://codeforces.com/blog/entry/18051
+	* KACTL
+ * Verification: SPOJ Fenwick
+ */
+
+tcT> struct SegTree { // cmb(ID,b) = b
+	// const T ID{}; T cmb(T a, T b) { return a+b; }
+    T ID{}; T cmb(T a, T b) { return a+b; }
+	int n; V<T> seg;
+	void init(int _n) { // upd, query also work if n = _n
+		for (n = 1; n < _n; ) n *= 2;
+		seg.assign(2*n,ID); }
+	void pull(int p) { seg[p] = cmb(seg[2*p],seg[2*p+1]); }
+	void upd(int p, T val) { // set val at position p
+		seg[p += n] = val; for (p /= 2; p; p /= 2) pull(p); }
+	T query(int l, int r) {	// zero-indexed, inclusive
+		T ra = ID, rb = ID;
+		for (l += n, r += n+1; l < r; l /= 2, r /= 2) {
+			if (l&1) ra = cmb(ra,seg[l++]);
+			if (r&1) rb = cmb(seg[--r],rb);
+		}
+		return cmb(ra,rb);
+	}
+	/// int first_at_least(int lo, int val, int ind, int l, int r) { // if seg stores max across range
+	/// 	if (r < lo || val > seg[ind]) return -1;
+	/// 	if (l == r) return l;
+	/// 	int m = (l+r)/2;
+	/// 	int res = first_at_least(lo,val,2*ind,l,m); if (res != -1) return res;
+	/// 	return first_at_least(lo,val,2*ind+1,m+1,r);
+	/// }
+};
+// /here goes the template!
+
+// here goes the work!
+struct node {
+    pair<long long, int> val;
+
+	node(): val(make_pair(-BIG, -79)) {}
+
+	node(pair<long long, int> _val) : val(_val) {}
+
+	node operator + (const node &rhs) const {
+		return node(max(val, rhs.val));
+	}
+};
+
+// /here goes the template!
+
+// here goes the work!
+SegTree<node> tree;
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    assert(0 < 2 * k && 2 * k <= n);
+
+    V<ll> X(n);
+    for(auto& x: X) cin >> x;
+
+    V<ll> pref = get_prefix_sums(X);
+
+    V<ll> ranges(n - k + 1);
+    for(int i = 0; i < n - k + 1; i++) {
+        ranges[i] = query(pref, i, i + k - 1);
+    }
+
+    // DBG(X);
+    // DBG(pref);
+    // DBG(ranges);
+
+    const int OFFSET = 12;
+    tree.init(n - k + 1 + OFFSET);
+
+    for(int i = 0; i < n - k + 1; i++) {
+        tree.upd(i, node(make_pair(ranges[i], -i))); //! -i
+    }
+
+    V<tuple<ll, int, int>> optimal;
+    for(int i = 0; i < n - k + 1; i++) {
+        if(i + k + k - 1>= n) break;
+
+        node best = tree.query(i + k, n - k);
+        optimal.eb(make_tuple(ranges[i] + best.val.first, -i, best.val.second));
+    }
+
+    sort(rall(optimal));
+
+    // for(auto& it: optimal) DBG3(get<0>(it), get<1>(it), get<2>(it));
+    auto it = optimal[0];
+    cout << (-get<1>(it) + 1) << " " << (-get<2>(it) + 1) << n_l;
+}
+
+// https://codeforces.com/contest/332/problem/B
+// B. Maximum Absurdity
+signed main() {
+    // read read read
+	setIO();
+    // cout << fixed << setprecision(12);
+
+    long long t = 1LL;
+    // cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    // should actually read the stuff at the bottom
+}
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+
+//! ERASE DBGs xd or TLE
