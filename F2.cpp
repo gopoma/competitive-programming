023d01e78673cf5558ac880c37691730834b3b63
gopoma <gordono@unsa.edu.pe>
2023-12-19 23:54:38 -0500

    good

--- F2.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-signed main() {
-    double valorDouble = 3.14;
-    int valorInt = static_cast<int>(valorDouble); // Conversión explícita de double a int
-    std::cout << valorInt << std::endl; // 3
-}
