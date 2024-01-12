6c29f0b8513ad4eceb9f3c7a5fe04d4ec00fdfaf
gopoma <gordono@unsa.edu.pe>
2024-01-12 00:05:08 -0500

    CSES

--- ZSES/DynamicProgramming/BookShop.cpp
+++ ZSES/DynamicProgramming/BookShop.cpp
@@ -271,96 +271,30 @@ const int dddy[8]{0, 1,  0, -1, 1, -1,  1, -1};
 
 //* here goes the template!
 /**
- * Description: modular arithmetic operations
- * Source:
- * KACTL
- * https://codeforces.com/blog/entry/63903
- * https://codeforces.com/contest/1261/submission/65632855 (tourist)
- * https://codeforces.com/contest/1264/submission/66344993 (ksun)
- * also see https://github.com/ecnerwala/cp-book/blob/master/src/modnum.hpp
- * (ecnerwal) Verification: https://open.kattis.com/problems/modulararithmetic
+ * Description: solves knapsack in pseudo-polynomial time
+ * Source: Own
+ * Verification: https://open.kattis.com/problems/knapsack
  */
 
-template <int MOD, int RT> struct mint {
-	static const int mod = MOD;
-	static constexpr mint rt() { return RT; }  // primitive root for FFT
-	int v;
-	explicit operator int() const {
-		return v;
-	}  // explicit -> don't silently convert to int
-	mint() : v(0) {}
-	mint(ll _v) {
-		v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
-		if (v < 0) v += MOD;
-	}
-	bool operator==(const mint &o) const { return v == o.v; }
-	friend bool operator!=(const mint &a, const mint &b) { return !(a == b); }
-	friend bool operator<(const mint &a, const mint &b) { return a.v < b.v; }
-	friend istream &operator>>(istream &is, mint &a) {
-		ll x;
-		is >> x;
-		a = mint(x);
-		return is;
-	}
-	friend ostream &operator<<(ostream &os, mint a) {
-		os << int(a);
-		return os;
-	}
-
-	mint &operator+=(const mint &o) {
-		if ((v += o.v) >= MOD) v -= MOD;
-		return *this;
-	}
-	mint &operator-=(const mint &o) {
-		if ((v -= o.v) < 0) v += MOD;
-		return *this;
-	}
-	mint &operator*=(const mint &o) {
-		v = int((ll)v * o.v % MOD);
-		return *this;
-	}
-	mint &operator/=(const mint &o) { return (*this) *= inv(o); }
-	friend mint pow(mint a, ll p) {
-		mint ans = 1;
-		assert(p >= 0);
-		for (; p; p /= 2, a *= a)
-			if (p & 1) ans *= a;
-		return ans;
-	}
-	friend mint inv(const mint &a) {
-		assert(a.v != 0);
-		return pow(a, MOD - 2);
-	}
-
-	mint operator-() const { return mint(-v); }
-	mint &operator++() { return *this += 1; }
-	mint &operator--() { return *this -= 1; }
-	friend mint operator+(mint a, const mint &b) { return a += b; }
-	friend mint operator-(mint a, const mint &b) { return a -= b; }
-	friend mint operator*(mint a, const mint &b) { return a *= b; }
-	friend mint operator/(mint a, const mint &b) { return a /= b; }
-};
-
-using mi = mint<MOD, 5>;  // 5 is primitive root for both common mods
-using vmi = V<mi>;
-using pmi = pair<mi, mi>;
-using vpmi = V<pmi>;
-
+vi solve(int cap, vi v, vi w) {
+	vi dp[sz(v)+1]; dp[0].rsz(cap+1);
+    F0R(i,sz(v)) {
+		dp[i+1] = dp[i];
+        F0R(j,cap+1-w[i]) ckmax(dp[i+1][w[i]+j],dp[i][j]+v[i]);
+    }
+    vi ans; R0F(i,sz(v)) if (dp[i][cap] != dp[i+1][cap]) cap -= w[i], ans.pb(i);
+    return ans;
+}
 //* /here goes the template!
 
 void solve() {
     def(int, n, x);
-    vi c(n); re(c);
+    vi h(n), s(n); re(h); re(s);
 
-    dbg(n, x, c);
+    vi go = solve(x, s, h);
 
-    vector<vector<mi>> dp(x, vector<mi>(n));
-    for(int j = 0; j < n; j++) dp[0][j] = 1;
-
-    for(int i = 1; i <= x; i++) {
-        for(int j = 0; j < n; j++) {
-        }
-    }
+    int ans = 0; each(idx, go) ans += s[idx];
+    ps(ans);
 }
 
 
