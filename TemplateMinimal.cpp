19065a9e200e80fdb73116f4a8c2300a1dde68de
gopoma <gordono@unsa.edu.pe>
2024-03-25 15:22:36 -0500

    CF

---
+++ TemplateMinimal.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "debugICPC.h"
+
+#define MACRO(code) do {code} while (false)
+#define RAYA MACRO(cerr << "\033[101m" << "================================" << "\033[0m" << endl;)
+#else
+#define dbg(...)
+
+#define RAYA
+#endif
+
+template<class T> using V = vector<T>;
+#define all(x) begin(x), end(x)
+
+void solve() {
+    dbg("xd");
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+    int t = 1;
+    //? cin >> t;
+
+    for(int idx = 0; idx < t; idx++) {
+        solve();
+    }
+}
