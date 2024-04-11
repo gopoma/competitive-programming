6c92e0403602e59fb089943df79e8b247099ddfc
gopoma <gordono@unsa.edu.pe>
2024-04-11 11:08:27 -0500

    CF

---
+++ SquareRoot.cpp
@@ -0,0 +1,323 @@
+//* sometimes pragmas don't work, if so, just comment it!
+#pragma GCC optimize ("Ofast")
+//? #pragma GCC target ("avx,avx2")
+//! #pragma GCC optimize ("trapv")
+
+#undef _GLIBCXX_DEBUG //? for Stress Testing
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debugICPC.h"
+
+#define chk(...) if (!(__VA_ARGS__)) cerr << "\033[41m" << "Line(" << __LINE__ << ") -> function(" \
+	 << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\033[0m" << "\n", exit(0);
+
+#define MACRO(code) do {code} while (false)
+#define RAYA MACRO(cerr << "\033[101m" << "================================" << "\033[0m" << endl;)
+#else
+#define dbg(...)
+
+#define chk(...)
+#define RAYA
+#endif
+
+const auto beg_time = std::chrono::high_resolution_clock::now();
+// https://stackoverflow.com/questions/47980498/accurate-c-c-clock-on-a-multi-core-processor-with-auto-overclock?noredirect=1&lq=1
+double time_elapsed() {
+	return chrono::duration<double>(std::chrono::high_resolution_clock::now() -
+	                                beg_time)
+	    .count();
+}
+
+
+
+// building blocks
+using ll  = long long;
+using db  = long double; // or double, if TL is tight
+using str = string;      // yay python!
+
+//? priority_queue for minimum
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+using ull  = unsigned long long;
+//? using i64  = long long;
+//? using u64  = uint64_t;
+//? using i128 = __int128;
+//? using u128 = __uint128_t;
+//? using f128 = __float128;
+
+
+
+// pairs
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+
+
+#define tcT template <class T
+#define tcTU tcT, class U
+//! ^ lol this makes everything look weird but I'll try it
+
+tcT > using V = vector<T>;
+tcT, size_t SZ > using AR = array<T, SZ>;
+using vi = V<int>;
+using vb = V<bool>;
+using vl = V<ll>;
+using vd = V<db>;
+using vs = V<str>;
+using vpi = V<pi>;
+using vpl = V<pl>;
+using vpd = V<pd>;
+
+// vectors
+// oops size(x), rbegin(x), rend(x) need C++17
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+#define ts to_string
+
+
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define rep(a) F0R(_,a)
+#define each(a,x) for (auto& a: x)
+
+
+
+const int MOD = 1e9+7;
+const ll BIG = 1e18;  //? not too close to LLONG_MAX
+const db PI = acos((db)-1);
+mt19937 rng(0); // or mt19937_64
+
+
+
+ll cdiv(ll a, ll b) {
+	return a / b + ((a ^ b) > 0 && a % b);
+}  // divide a by b rounded up
+ll fdiv(ll a, ll b) {
+	return a / b - ((a ^ b) < 0 && a % b);
+}  // divide a by b rounded down
+
+tcT> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+
+tcT > void remDup(vector<T> &v) {  // sort and remove duplicates
+	sort(all(v));
+	v.erase(unique(all(v)), end(v));
+}
+tcTU > void safeErase(T &t, const U &u) {
+	auto it = t.find(u);
+	assert(it != end(t));
+	t.erase(it);
+}
+
+
+
+inline namespace FileIO {
+void setIn(str s) { freopen(s.c_str(), "r", stdin); }
+void setOut(str s) { freopen(s.c_str(), "w", stdout); }
+void setIO(str s = "") {
+	cin.tie(0)->sync_with_stdio(0);  // unsync C / C++ I/O streams
+	//? cout << fixed << setprecision(12);
+    //? cerr << fixed << setprecision(12);
+	cin.exceptions(cin.failbit);
+	// throws exception when do smth illegal
+	// ex. try to read letter into int
+	if (sz(s)) setIn(s + ".in"), setOut(s + ".out");  // for old USACO
+}
+}  // namespace FileIO
+
+
+
+//? Custom Helpers
+template <typename T>
+inline T gcd(T a, T b) { while (b != 0) swap(b, a %= b); return a; }
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+//? /Custom Helpers
+
+/**
+ * Description: Barrett reduction computes $a \% b$ about 4 times faster than usual
+ 	* where $b>1$ is constant but not known at compile time. Division by $b$ is replaced
+ 	* by multiplication by $m$ and shifting right 64 bits.
+ * Source: KACTL
+ 	* https://github.com/kth-competitive-programming/kactl/blob/master/content/various/FastMod.h
+ 	* https://en.wikipedia.org/wiki/Barrett_reduction
+ * Verification: http://www.usaco.org/index.php?page=viewproblem2&cpid=1045
+ */
+
+using ul = uint64_t; using L = __uint128_t;
+struct FastMod {
+	ul b, m; FastMod(ul b) : b(b), m(-1ULL / b) {}
+	ul reduce(ul a) {
+		ul q = (ul)((__uint128_t(m) * a) >> 64), r = a - q * b;
+		return r - (r >= b) * b; }
+};
+
+/**
+ * Description: modular arithmetic operations
+ * Source:
+	* KACTL
+	* https://codeforces.com/blog/entry/63903
+	* https://codeforces.com/contest/1261/submission/65632855 (tourist)
+	* https://codeforces.com/contest/1264/submission/66344993 (ksun)
+	* also see https://github.com/ecnerwala/cp-book/blob/master/src/modnum.hpp (ecnerwal)
+	* https://github.com/atcoder/ac-library/blob/master/atcoder/modint.hpp
+ * Verification:
+	* https://open.kattis.com/problems/modulararithmetic
+ */
+
+template<int id> struct mint {
+	static FastMod MF;
+	static int mod() { return (int)MF.b; }
+	static void set_mod(int m) { assert(1 <= m); MF = FastMod(m); }
+	int v; explicit operator int() const { return v; } // explicit -> don't silently convert to int
+	mint() { v = 0; }
+	mint(ll _v) { v = int((-mod() < _v && _v < mod()) ? _v : _v % mod());
+		if (v < 0) v += mod(); }
+	friend bool operator==(const mint& a, const mint& b) {
+		return a.v == b.v; }
+	friend bool operator!=(const mint& a, const mint& b) {
+		return !(a == b); }
+	friend bool operator<(const mint& a, const mint& b) {
+		return a.v < b.v; }
+	//? friend void re(mint& a) { ll x; re(x); a = mint(x); }
+	friend str ts(mint a) { return ts(a.v); }
+
+	mint& operator+=(const mint& m) {
+		if ((v += m.v) >= mod()) v -= mod();
+		return *this; }
+	mint& operator-=(const mint& m) {
+		if ((v -= m.v) < 0) v += mod();
+		return *this; }
+	mint& operator*=(const mint& m) {
+		v = (int)MF.reduce((ll)v*m.v); return *this; }
+	mint& operator/=(const mint& m) { return (*this) *= inv(m); }
+	friend mint pow(mint a, ll p) {
+		mint ans = 1; assert(p >= 0);
+		for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+		return ans; }
+	friend mint inv(const mint& a) { assert(a.v != 0);
+		return pow(a,mod()-2); }
+
+	mint operator-() const { return mint(-v); }
+	mint& operator++() { return *this += 1; }
+	mint& operator--() { return *this -= 1; }
+	friend mint operator+(mint a, const mint& b) { return a += b; }
+	friend mint operator-(mint a, const mint& b) { return a -= b; }
+	friend mint operator*(mint a, const mint& b) { return a *= b; }
+	friend mint operator/(mint a, const mint& b) { return a /= b; }
+};
+template<int id> FastMod mint<id>::MF = FastMod(1);
+
+typedef mint<0> mi; // 5 is primitive root for both common mods
+typedef vector<mi> vmi;
+typedef pair<mi,mi> pmi;
+typedef vector<pmi> vpmi;
+
+vector<vmi> scmb; // small combinations
+void genComb(int SZ) {
+	scmb.assign(SZ,vmi(SZ)); scmb[0][0] = 1;
+	FOR(i,1,SZ) F0R(j,i+1)
+		scmb[i][j] = scmb[i-1][j]+(j?scmb[i-1][j-1]:0);
+}
+
+/**
+ * Description: Tonelli-Shanks algorithm for square roots mod a prime. -1 if doesn't exist.
+ * Time: O(\log^2(MOD))
+ * Source: http://www.math.vt.edu/people/brown/class_homepages/shanks_tonelli.pdf
+ * Verification: https://www.spoj.com/problems/CRYPTO1
+ * Usage: sqrt(mi((ll)1e10)); // 100000
+ */
+
+using T = int;
+T sqrt(mi a) {
+    const int MOD = mi::mod();
+
+	mi p = pow(a,(MOD-1)/2);
+	if (p.v != 1) return p.v == 0 ? 0 : -1;
+	/// check if 0 or no sqrt
+	T s = MOD-1; int r = 0; while (s%2 == 0) s /= 2, ++r;
+	mi n = 2; while (pow(n,(MOD-1)/2).v == 1) n = T(n)+1;
+	// n non-square, ord(g)=2^r, ord(b)=2^m, ord(g)=2^r, m<r
+	for (mi x = pow(a,(s+1)/2), b = pow(a,s), g = pow(n,s);;) {
+		if (b.v == 1) return min(x.v,MOD-x.v); // x^2=ab
+		int m = 0; for (mi t = b; t.v != 1; t *= t) ++m;
+		rep(r-m-1) g *= g; // ord(g)=2^{m+1}
+		x *= g, g *= g, b *= g, r = m; // ord(g)=2^m, ord(b)<2^m
+	}
+}
+
+void solve() {
+    int a, n; cin >> a >> n;
+
+    mi::set_mod(n);
+
+    int ans = sqrt(mi(a));
+
+    dbg(ans);
+
+    if(ans == -1) cout << "No root\n";
+    else {
+        int val1 = ans;
+        int val2 = n - ans;
+
+        assert(val1 != val2);
+
+        cout << val1 << " " << val2 << "\n";
+    }
+
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t = 1;
+    cin >> t;
+
+    for(int idx = 0; idx < t; idx++) {
+        RAYA;
+        RAYA;
+        solve();
+    }
+    RAYA;
+    RAYA;
+
+    #ifdef LOCAL
+        cerr << fixed << setprecision(5);
+        cerr << "\033[42m++++++++++++++++++++\033[0m\n";
+        cerr << "\033[42mtime = " << time_elapsed() << "ms\033[0m\n";
+        cerr << "\033[42m++++++++++++++++++++\033[0m";
+    #endif
+}
