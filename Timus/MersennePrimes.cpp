ea82ddd6130c97658f2e33225c10e977f884d229
gopoma <gordono@unsa.edu.pe>
2024-07-04 00:28:59 -0500

    Timus

---
+++ Timus/MersennePrimes.cpp
@@ -0,0 +1,422 @@
+//* sometimes pragmas don't work, if so, just comment it!
+//? #pragma GCC optimize ("Ofast")
+//? #pragma GCC target ("avx,avx2")
+//! #pragma GCC optimize ("trapv")
+
+//! #undef _GLIBCXX_DEBUG //? for Stress Testing
+
+#include <bits/stdc++.h> //? if you don't want IntelliSense
+
+using namespace std;
+
+// building blocks
+using ll  = long long;
+using db  = long double; // or double, if TL is tight
+using str = string;      // yay python!
+
+//? priority_queue for minimum
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+using ull  = unsigned long long;
+//? using i64  = long long;
+//? using u64  = uint64_t;
+//? using i128 = __int128;
+//? using u128 = __uint128_t;
+//? using f128 = __float128;
+
+
+
+// pairs
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+
+#define mp make_pair
+#define f  first
+#define s  second
+
+
+
+#define tcT template <class T
+#define tcTU tcT, class U
+//! ^ lol this makes everything look weird but I'll try it
+
+tcT > using V = vector<T>;
+tcT, size_t SZ > using AR = array<T, SZ>;
+using vi = V<int>;
+using vb = V<bool>;
+using vl = V<ll>;
+using vd = V<db>;
+using vs = V<str>;
+using vpi = V<pi>;
+using vpl = V<pl>;
+using vpd = V<pd>;
+
+// vectors
+// oops size(x), rbegin(x), rend(x) need C++17
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+#define ts to_string
+
+#define lb lower_bound
+#define ub upper_bound
+tcT > int lwb(V<T> &a, const T &b) { return int(lb(all(a), b) - bg(a)); }
+tcT > int upb(V<T> &a, const T &b) { return int(ub(all(a), b) - bg(a)); }
+
+
+
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+
+
+const int MOD = 1e9 + 7;
+const int MX = (int)2e5 + 5;
+const ll BIG = 1e18;  //? not too close to LLONG_MAX
+const db PI = acos((db)-1);
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};  //? for every grid problem!!
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+
+
+// bitwise ops
+// also see https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+constexpr int pct(int x) { return __builtin_popcount(x); }  // # of bits set
+constexpr int bits(int x) {  // assert(x >= 0); // make C++11 compatible until
+	                         // USACO updates ...
+	return x == 0 ? 0 : 31 - __builtin_clz(x);
+}  // floor(log2(x))
+constexpr int p2(int x) { return 1 << x; }
+constexpr int msk2(int x) { return p2(x) - 1; }
+
+ll cdiv(ll a, ll b) {
+	return a / b + ((a ^ b) > 0 && a % b);
+}  // divide a by b rounded up
+ll fdiv(ll a, ll b) {
+	return a / b - ((a ^ b) < 0 && a % b);
+}  // divide a by b rounded down
+
+tcT > bool ckmin(T &a, const T &b) {
+	return b < a ? a = b, 1 : 0;
+}  // set a = min(a,b)
+tcT > bool ckmax(T &a, const T &b) {
+	return a < b ? a = b, 1 : 0;
+}  // set a = max(a,b)
+
+tcTU > T fstTrue(T lo, T hi, U f) {
+	++hi;
+	assert(lo <= hi);  // assuming f is increasing
+	while (lo < hi) {  // find first index such that f is true
+		T mid = lo + (hi - lo) / 2;
+		f(mid) ? hi = mid : lo = mid + 1;
+	}
+	return lo;
+}
+tcTU > T lstTrue(T lo, T hi, U f) {
+	--lo;
+	assert(lo <= hi);  // assuming f is decreasing
+	while (lo < hi) {  // find first index such that f is true
+		T mid = lo + (hi - lo + 1) / 2;
+		f(mid) ? lo = mid : hi = mid - 1;
+	}
+	return lo;
+}
+tcT > void remDup(vector<T> &v) {  // sort and remove duplicates
+	sort(all(v));
+	v.erase(unique(all(v)), end(v));
+}
+tcTU > void safeErase(T &t, const U &u) {
+	auto it = t.find(u);
+	assert(it != end(t));
+	t.erase(it);
+}
+
+
+
+#define tcTUU tcT, class ...U
+
+inline namespace IO {
+#define SFINAE(x, ...)                                                         \
+	template <class, class = void> struct x : std::false_type {};              \
+	template <class T> struct x<T, std::void_t<__VA_ARGS__>> : std::true_type {}
+
+SFINAE(DefaultI, decltype(std::cin >> std::declval<T &>()));
+SFINAE(DefaultO, decltype(std::cout << std::declval<T &>()));
+SFINAE(IsTuple, typename std::tuple_size<T>::type);
+SFINAE(Iterable, decltype(std::begin(std::declval<T>())));
+
+template <auto &is> struct Reader {
+	template <class T> void Impl(T &t) {
+		if constexpr (DefaultI<T>::value) is >> t;
+		else if constexpr (Iterable<T>::value) {
+			for (auto &x : t) Impl(x);
+		} else if constexpr (IsTuple<T>::value) {
+			std::apply([this](auto &...args) { (Impl(args), ...); }, t);
+		} else static_assert(IsTuple<T>::value, "No matching type for read");
+	}
+	template <class... Ts> void read(Ts &...ts) { ((Impl(ts)), ...); }
+};
+
+template <class... Ts> void re(Ts &...ts) { Reader<cin>{}.read(ts...); }
+#define def(t, args...)                                                        \
+	t args;                                                                    \
+	re(args);
+
+template <auto &os, bool debug, bool print_nd> struct Writer {
+	string comma() const { return debug ? "," : ""; }
+	template <class T> constexpr char Space(const T &) const {
+		return print_nd && (Iterable<T>::value or IsTuple<T>::value) ? '\n'
+		                                                             : ' ';
+	}
+	template <class T> void Impl(T const &t) const {
+		if constexpr (DefaultO<T>::value) os << t;
+		else if constexpr (Iterable<T>::value) {
+			if (debug) os << '{';
+			int i = 0;
+			for (auto &&x : t)
+				((i++) ? (os << comma() << Space(x), Impl(x)) : Impl(x));
+			if (debug) os << '}';
+		} else if constexpr (IsTuple<T>::value) {
+			if (debug) os << '(';
+			std::apply(
+			    [this](auto const &...args) {
+				    int i = 0;
+				    (((i++) ? (os << comma() << " ", Impl(args)) : Impl(args)),
+				     ...);
+			    },
+			    t);
+			if (debug) os << ')';
+		} else static_assert(IsTuple<T>::value, "No matching type for print");
+	}
+	template <class T> void ImplWrapper(T const &t) const {
+		if (debug) os << "\033[0;31m";
+		Impl(t);
+		if (debug) os << "\033[0m";
+	}
+	template <class... Ts> void print(Ts const &...ts) const {
+		((Impl(ts)), ...);
+	}
+	template <class F, class... Ts>
+	void print_with_sep(const std::string &sep, F const &f,
+	                    Ts const &...ts) const {
+		ImplWrapper(f), ((os << sep, ImplWrapper(ts)), ...), os << '\n';
+	}
+	void print_with_sep(const std::string &) const { os << '\n'; }
+};
+
+template <class... Ts> void pr(Ts const &...ts) {
+	Writer<cout, false, true>{}.print(ts...);
+}
+template <class... Ts> void ps(Ts const &...ts) {
+	Writer<cout, false, true>{}.print_with_sep(" ", ts...);
+}
+}  // namespace IO
+
+inline namespace Debug {
+
+#ifdef LOCAL
+#include "helpers/debug.h"
+
+#define chk(...) if (!(__VA_ARGS__)) cerr << "\033[41m" << "Line(" << __LINE__ << ") -> function(" \
+	 << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\033[0m" << "\n", exit(0);
+
+#define MACRO(code) do {code} while (false)
+#define RAYA MACRO(cerr << "\033[101m" << "================================" << "\033[0m" << endl;)
+#else
+#define dbg(...)
+
+#define chk(...)
+#define RAYA
+#endif
+
+const auto beg_time = std::chrono::high_resolution_clock::now();
+// https://stackoverflow.com/questions/47980498/accurate-c-c-clock-on-a-multi-core-processor-with-auto-overclock?noredirect=1&lq=1
+double time_elapsed() {
+	return chrono::duration<double>(std::chrono::high_resolution_clock::now() -
+	                                beg_time)
+	    .count();
+}
+}  // namespace Debug
+
+
+
+inline namespace FileIO {
+void setIn(str s) { freopen(s.c_str(), "r", stdin); }
+void setOut(str s) { freopen(s.c_str(), "w", stdout); }
+void setIO(str s = "") {
+	cin.tie(0)->sync_with_stdio(0);  // unsync C / C++ I/O streams
+	//? cout << fixed << setprecision(12);
+    //? cerr << fixed << setprecision(12);
+	cin.exceptions(cin.failbit);
+	// throws exception when do smth illegal
+	// ex. try to read letter into int
+	if (sz(s)) setIn(s + ".in"), setOut(s + ".out");  // for old USACO
+}
+}  // namespace FileIO
+
+
+
+//? Custom Helpers
+template <typename T>
+inline T gcd(T a, T b) { while (b != 0) swap(b, a %= b); return a; }
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+const int dddx[8]{1, 0, -1,  0, 1,  1, -1, -1};
+const int dddy[8]{0, 1,  0, -1, 1, -1,  1, -1};
+
+//? /Custom Helpers
+
+
+
+//* Template
+/**
+ * Description: Deterministic primality test, works up to $2^{64}$.
+ 	* For larger numbers, extend $A$ randomly.
+ * Source: KACTL
+	* https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
+ * Verification: https://www.spoj.com/problems/FACT0/
+ */
+
+/**
+ * Description: Multiply two 64-bit integers mod another if 128-bit is not available.
+	* modMul is equivalent to \texttt{(ul)(\_\_int128(a)*b\%mod)}.
+	* Works for $0\le a,b<mod<2^{63}.$
+ * Source: KACTL
+ * Verification: see "Faster Factoring"
+ */
+
+/// using db = long double;
+using ul = uint64_t;
+ul modMul(ul a, ul b, const ul mod) {
+	ll ret = a*b-mod*(ul)((db)a*b/mod);
+	return ret+((ret<0)-(ret>=(ll)mod))*mod; }
+ul modPow(ul a, ul b, const ul mod) {
+	if (b == 0) return 1;
+	ul res = modPow(a,b/2,mod); res = modMul(res,res,mod);
+	return b&1 ? modMul(res,a,mod) : res;
+}
+
+bool prime(ul n) { // not ll!
+	if (n < 2 || n % 6 % 4 != 1) return n-2 < 2;
+	ul A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
+	    s = __builtin_ctzll(n-1), d = n>>s;
+	each(a,A) {   // ^ count trailing zeroes
+		ul p = modPow(a,d,n), i = s;
+		while (p != 1 && p != n-1 && a%n && i--) p = modMul(p,p,n);
+		if (p != n-1 && i != s) return 0;
+	}
+	return 1;
+}
+//* /Template
+
+map<ll, ll> ans;
+void precompute() {
+    ll idx = 1;
+    for(ll N = 1; N <= 31; N++) {
+        if(prime((1LL << N) - 1LL)) {
+            ans[idx++] = N;
+        }
+    }
+    ans[9]  = 61;
+    ans[10] = 89;
+    ans[11] = 107;
+    ans[12] = 127;
+
+    ans[13] = 521;
+    ans[14] = 607;
+    ans[15] = 1279;
+    ans[16] = 2203;
+    ans[17] = 2281;
+
+    ans[23] = 11213;
+    ans[25] = 21701;
+    ans[26] = 23209;
+    ans[31] = 216091;
+
+    ans[35] = 1398269;
+    ans[36] = 2976221;
+    ans[37] = 3021377;
+    ans[38] = 6972593;
+
+    //?
+    {
+        vl keys{18, 32, 24, 21, 19, 34, 27, 33, 20, 30, 28, 29, 22,};
+        vl values{3217, 756839, 19937, 9689, 4253, 1257787, 44497, 859433, 4423, 132049, 86243, 110503, 9941,};
+        for(int i = 0; i < sz(keys); i++) ans[keys[i]] = values[i];
+    }
+    //?
+
+    each(x, ans) dbg(x);
+    vl R;
+    for(ll idx = 1; idx <= 38; idx++) if(!ans.count(idx)) R.eb(idx);
+    dbg("Falta", R);
+}
+
+void solve() {
+    def(ll, N);
+    ps(ans[N]);
+}
+
+
+//? Generator
+int rng_int(int L, int R) { assert(L <= R);
+	return uniform_int_distribution<int>(L,R)(rng);  }
+ll rng_ll(ll L, ll R) { assert(L <= R);
+	return uniform_int_distribution<ll>(L,R)(rng);  }
+//? /Generator
+
+
+signed main() {
+    setIO();
+
+    precompute();
+
+    ll t = 1; re(t);
+
+    FOR(i, 1, t + 1) {
+        RAYA;
+        RAYA;
+        solve();
+    }
+    RAYA;
+    RAYA;
+
+    #ifdef LOCAL
+        cerr << fixed << setprecision(5);
+        cerr << "\033[42m++++++++++++++++++++\033[0m\n";
+        cerr << "\033[42mtime = " << time_elapsed() << "ms\033[0m\n";
+        cerr << "\033[42m++++++++++++++++++++\033[0m";
+    #endif
+}
+
+/* stuff you should look for
+ * int overflow, array bounds
+ * special cases (n=1?)
+ * do smth instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
