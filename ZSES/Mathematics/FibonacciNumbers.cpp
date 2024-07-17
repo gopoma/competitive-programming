027d3b8f8abba53dd150a1b9e3eab974d3ce4395
gopoma <gordono@unsa.edu.pe>
2024-07-16 20:00:55 -0500

    CSES

--- ZSES/Mathematics/FibonacciNumbers.cpp
+++ ZSES/Mathematics/FibonacciNumbers.cpp
@@ -9,15 +9,24 @@
 
 using namespace std;
 
+#ifdef LOCAL
+    #include "helpers/debug.h"
+#else
+    #define dbg(...)     0
+    #define chk(...)     0
+
+    #define RAYA         0
+#endif
+
 // building blocks
 using ll  = long long;
 using db  = long double; // or double, if TL is tight
 using str = string;      // yay python!
 
 //? priority_queue for minimum
-template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+//? template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
 
-using ull  = unsigned long long;
+//? using ull  = unsigned long long;
 //? using i64  = long long;
 //? using u64  = uint64_t;
 //? using i128 = __int128;
@@ -149,100 +158,6 @@ tcTU > void safeErase(T &t, const U &u) {
 
 #define tcTUU tcT, class ...U
 
-inline namespace IO {
-#define SFINAE(x, ...)                                                         \
-	template <class, class = void> struct x : std::false_type {};              \
-	template <class T> struct x<T, std::void_t<__VA_ARGS__>> : std::true_type {}
-
-SFINAE(DefaultI, decltype(std::cin >> std::declval<T &>()));
-SFINAE(DefaultO, decltype(std::cout << std::declval<T &>()));
-SFINAE(IsTuple, typename std::tuple_size<T>::type);
-SFINAE(Iterable, decltype(std::begin(std::declval<T>())));
-
-template <auto &is> struct Reader {
-	template <class T> void Impl(T &t) {
-		if constexpr (DefaultI<T>::value) is >> t;
-		else if constexpr (Iterable<T>::value) {
-			for (auto &x : t) Impl(x);
-		} else if constexpr (IsTuple<T>::value) {
-			std::apply([this](auto &...args) { (Impl(args), ...); }, t);
-		} else static_assert(IsTuple<T>::value, "No matching type for read");
-	}
-	template <class... Ts> void read(Ts &...ts) { ((Impl(ts)), ...); }
-};
-
-template <class... Ts> void re(Ts &...ts) { Reader<cin>{}.read(ts...); }
-#define def(t, args...)                                                        \
-	t args;                                                                    \
-	re(args);
-
-template <auto &os, bool debug, bool print_nd> struct Writer {
-	string comma() const { return debug ? "," : ""; }
-	template <class T> constexpr char Space(const T &) const {
-		return print_nd && (Iterable<T>::value or IsTuple<T>::value) ? '\n'
-		                                                             : ' ';
-	}
-	template <class T> void Impl(T const &t) const {
-		if constexpr (DefaultO<T>::value) os << t;
-		else if constexpr (Iterable<T>::value) {
-			if (debug) os << '{';
-			int i = 0;
-			for (auto &&x : t)
-				((i++) ? (os << comma() << Space(x), Impl(x)) : Impl(x));
-			if (debug) os << '}';
-		} else if constexpr (IsTuple<T>::value) {
-			if (debug) os << '(';
-			std::apply(
-			    [this](auto const &...args) {
-				    int i = 0;
-				    (((i++) ? (os << comma() << " ", Impl(args)) : Impl(args)),
-				     ...);
-			    },
-			    t);
-			if (debug) os << ')';
-		} else static_assert(IsTuple<T>::value, "No matching type for print");
-	}
-	template <class T> void ImplWrapper(T const &t) const {
-		if (debug) os << "\033[0;31m";
-		Impl(t);
-		if (debug) os << "\033[0m";
-	}
-	template <class... Ts> void print(Ts const &...ts) const {
-		((Impl(ts)), ...);
-	}
-	template <class F, class... Ts>
-	void print_with_sep(const std::string &sep, F const &f,
-	                    Ts const &...ts) const {
-		ImplWrapper(f), ((os << sep, ImplWrapper(ts)), ...), os << '\n';
-	}
-	void print_with_sep(const std::string &) const { os << '\n'; }
-};
-
-template <class... Ts> void pr(Ts const &...ts) {
-	Writer<cout, false, true>{}.print(ts...);
-}
-template <class... Ts> void ps(Ts const &...ts) {
-	Writer<cout, false, true>{}.print_with_sep(" ", ts...);
-}
-}  // namespace IO
-
-inline namespace Debug {
-
-#ifdef LOCAL
-#include "helpers/debug.h"
-
-#define chk(...) if (!(__VA_ARGS__)) cerr << "\033[41m" << "Line(" << __LINE__ << ") -> function(" \
-	 << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\033[0m" << "\n", exit(0);
-
-#define MACRO(code) do {code} while (false)
-#define RAYA MACRO(cerr << "\033[101m" << "================================" << "\033[0m" << endl;)
-#else
-#define dbg(...)
-
-#define chk(...)
-#define RAYA
-#endif
-
 const auto beg_time = std::chrono::high_resolution_clock::now();
 // https://stackoverflow.com/questions/47980498/accurate-c-c-clock-on-a-multi-core-processor-with-auto-overclock?noredirect=1&lq=1
 double time_elapsed() {
@@ -250,9 +165,6 @@ double time_elapsed() {
 	                                beg_time)
 	    .count();
 }
-}  // namespace Debug
-
-
 
 inline namespace FileIO {
 void setIn(str s) { freopen(s.c_str(), "r", stdin); }
@@ -293,49 +205,144 @@ const int dddy[8]{0, 1,  0, -1, 1, -1,  1, -1};
 
 
 //* Template
-using Matrix = array<array<ll, 2>, 2>;
 
-Matrix mul(Matrix a, Matrix b) {
-	Matrix res = {{{0, 0}, {0, 0}}};
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 2; j++) {
-			for (int k = 0; k < 2; k++) {
-				res[i][j] += a[i][k] * b[k][j];
-				res[i][j] %= MOD; // TODO: Be Careful!
-			}
-		}
+/**
+ * Description: modular arithmetic operations
+ * Source:
+ * KACTL
+ * https://codeforces.com/blog/entry/63903
+ * https://codeforces.com/contest/1261/submission/65632855 (tourist)
+ * https://codeforces.com/contest/1264/submission/66344993 (ksun)
+ * also see https://github.com/ecnerwala/cp-book/blob/master/src/modnum.hpp
+ * (ecnerwal) Verification: https://open.kattis.com/problems/modulararithmetic
+ */
+
+template <int MOD, int RT> struct mint {
+	static const int mod = MOD;
+	static constexpr mint rt() { return RT; }  // primitive root for FFT
+	int v;
+	explicit operator int() const {
+		return v;
+	}  // explicit -> don't silently convert to int
+	mint() : v(0) {}
+	mint(ll _v) {
+		v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
+		if (v < 0) v += MOD;
+	}
+	bool operator==(const mint &o) const { return v == o.v; }
+	friend bool operator!=(const mint &a, const mint &b) { return !(a == b); }
+	friend bool operator<(const mint &a, const mint &b) { return a.v < b.v; }
+	friend istream &operator>>(istream &is, mint &a) {
+		ll x;
+		is >> x;
+		a = mint(x);
+		return is;
+	}
+	friend ostream &operator<<(ostream &os, mint a) {
+		os << int(a);
+		return os;
 	}
 
+	mint &operator+=(const mint &o) {
+		if ((v += o.v) >= MOD) v -= MOD;
+		return *this;
+	}
+	mint &operator-=(const mint &o) {
+		if ((v -= o.v) < 0) v += MOD;
+		return *this;
+	}
+	mint &operator*=(const mint &o) {
+		v = int((ll)v * o.v % MOD);
+		return *this;
+	}
+	mint &operator/=(const mint &o) { return (*this) *= inv(o); }
+	friend mint pow(mint a, ll p) {
+		mint ans = 1;
+		assert(p >= 0);
+		for (; p; p /= 2, a *= a)
+			if (p & 1) ans *= a;
+		return ans;
+	}
+	friend mint inv(const mint &a) {
+		assert(a.v != 0);
+		return pow(a, MOD - 2);
+	}
+
+	mint operator-() const { return mint(-v); }
+	mint &operator++() { return *this += 1; }
+	mint &operator--() { return *this -= 1; }
+	friend mint operator+(mint a, const mint &b) { return a += b; }
+	friend mint operator-(mint a, const mint &b) { return a -= b; }
+	friend mint operator*(mint a, const mint &b) { return a *= b; }
+	friend mint operator/(mint a, const mint &b) { return a /= b; }
+};
+
+using mi = mint<MOD, 5>;  // 5 is primitive root for both common mods
+using vmi = V<mi>;
+using pmi = pair<mi, mi>;
+using vpmi = V<pmi>;
+
+/**
+ * Description: 2D matrix operations.
+ * Source: KACTL
+ * Verification: https://dmoj.ca/problem/si17c1p5, SPOJ MIFF
+ */
+
+using T = mi;
+using Mat = V<V<T>>; // use array instead if tight TL
+
+Mat makeMat(int r, int c) { return Mat(r,V<T>(c)); }
+Mat makeId(int n) {
+	Mat m = makeMat(n,n); F0R(i,n) m[i][i] = 1;
+	return m;
+}
+/// Mat& operator+=(Mat& a, const Mat& b) {
+/// 	assert(sz(a) == sz(b) && sz(a[0]) == sz(b[0]));
+/// 	F0R(i,sz(a)) F0R(j,sz(a[0])) a[i][j] += b[i][j];
+/// 	return a;
+/// }
+/// Mat& operator-=(Mat& a, const Mat& b) {
+/// 	assert(sz(a) == sz(b) && sz(a[0]) == sz(b[0]));
+/// 	F0R(i,sz(a)) F0R(j,sz(a[0])) a[i][j] -= b[i][j];
+/// 	return a;
+/// }
+/// Mat operator+(Mat a, const Mat& b) { return a += b; }
+/// Mat operator-(Mat a, const Mat& b) { return a -= b; }
+/// V<T> operator*(const Mat& l, const V<T>& r) {
+/// 	assert(sz(l[0]) == sz(r));
+/// 	V<T> ret(sz(l));
+/// 	F0R(i,sz(l)) F0R(j,sz(l[0])) ret[i] += l[i][j]*r[j];
+/// 	return ret;
+/// }
+Mat operator*(const Mat& a, const Mat& b) {
+	int x = sz(a), y = sz(a[0]), z = sz(b[0]);
+	assert(y == sz(b)); Mat c = makeMat(x,z);
+	F0R(i,x) F0R(j,y) F0R(k,z) c[i][k] += a[i][j]*b[j][k];
+	return c;
+}
+Mat& operator*=(Mat& a, const Mat& b) { return a = a*b; }
+Mat pow(Mat m, ll p) {
+	int n = sz(m); assert(n == sz(m[0]) && p >= 0);
+	Mat res = makeId(n);
+	for (; p; p /= 2, m *= m) if (p&1) res *= m;
 	return res;
 }
-
-Matrix power(Matrix a, ll n) {
-    Matrix base = {{{1, 0}, {0, 1}}};
-
-    if(n == 0) return base;
-    else {
-        Matrix result = base;
-        Matrix m = a;
-
-        for (; n > 0; n /= 2, m = mul(m, m)) {
-            if (n & 1) result = mul(result, m);
-        }
-
-        return result;
-    }
-
-}
-
 //* /Template
 
 void solve() {
-    def(ll, n);
-    dbg(n);
+    ll n; cin >> n;
+    Mat base = makeMat(2, 2);
 
-    Matrix act = {{{1, 1}, {1, 0}}};
-    Matrix ans = power(act, n);
+    base[0][0] = 1;
+    base[0][1] = 1;
+    base[1][0] = 1;
+    base[1][1] = 0;
 
-    ps(ans[0][1]);
+    Mat mtx = pow(base, n);
+    mi ans = mtx[0][1];
+    assert(ans == mtx[1][0]);
+
+    cout << ans << "\n";
 }
 
 
@@ -350,7 +357,8 @@ ll rng_ll(ll L, ll R) { assert(L <= R);
 signed main() {
     setIO();
 
-    ll t = 1; //? re(t);
+    ll t = 1;
+    //? cin >> t;
 
     FOR(i, 1, t + 1) {
         RAYA;
