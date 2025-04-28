9784a509cbc849ec87533a5d559ad7806102a924
gopoma <gordono@unsa.edu.pe>
2025-04-27 22:53:53 -0500

    icpc

---
+++ template/icpc/temc.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//* Debugger
+string to_string(string s) {
+    return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+    return to_string((string) s);
+}
+
+string to_string(bool b) {
+    return (b ? "true" : "false");
+}
+
+template <typename A>
+string to_string(A v);
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "[";
+    for (const auto &x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "]";
+    return res;
+}
+//* /Debugger
+
+using ll = long long;
+using db = long double; // or double if tight TL
+using str = string;
+
+using pi = pair<int,int>;
+#define mp make_pair
+#define f first
+#define s second
+
+#define tcT template<class T
+tcT> using V = vector<T>;
+tcT, size_t SZ> using AR = array<T,SZ>;
+using vi = V<int>;
+using vb = V<bool>;
+using vpi = V<pi>;
+
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+#define sor(x) sort(all(x))
+#define rsz resize
+#define pb push_back
+#define ft front()
+#define bk back()
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define rep(a) F0R(_,a)
+#define each(a,x) for (auto& a: x)
+
+const int MOD = 1e9+7;
+const db PI = acos((db)-1);
+mt19937 rng(0); // or mt19937_64
+
+tcT> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+
+#define dbg(x) cout << "Line(" << __LINE__ << "): " << "\033[1;34m" << #x << "\033[0;32m = \033[35m" << to_string(x) << "\033[0m" << endl;
+#define GA dbg(0)
+#define RAYA   dbg("================================");
+
+void setIn(string s) { freopen(s.c_str(), "r", stdin); }
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+}
