77c9a99ff4e024c0c3f152749c09704ee3c356fe
gopoma <gordono@unsa.edu.pe>
2025-04-27 22:34:14 -0500

    icpc

--- template/icpc/temh.cpp
+++ template/icpc/temh.cpp
@@ -57,46 +57,4 @@ void setIn(string s) { freopen(s.c_str(), "r", stdin); }
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
-    vl  A = {1, -2, 3, -4, 5, -6};
-    V<long double>  B = {1.075, 2.75};
-    V<pl> C = {make_pair(-25, 75), make_pair(79, 13)};
-    map<ll, int> D; D[12]++; D[13] = ll(1e9) + 5;
-    unordered_map<ll, int> E; E[12] = ll(1e9) + 5; E[13]++;
-    set<ll> F; F.emplace(23); F.emplace(23); F.emplace(16);
-    unordered_set<ll> G; G.emplace(23); G.emplace(23); F.emplace(16);
-    multiset<ll> H; H.emplace(23); H.emplace(23); H.emplace(16);
-    string I = "xd";
-
-    dbg(C);
-
-    const int n = 3;
-    const int m = 4;
-    const int k = 5;
-    V<V<vl>> dp(n, V<vl>(m, vl(k)));
-
-    array<int, 4> ndp = {1, 2, 3, 4};
-
-    dbg(A);
-    dbg(B);
-    dbg(dp);
-    dbg(1);
-    dbg("dhuamanilu");
-    GA;
-    dbg(ndp);
-    RAYA;
-    set<int> xd;
-    for(int x = 1; x <= 10; x++) xd.emplace(x);
-    dbg(xd);
-
-    map<pair<int, int>, vector<int>> mp;
-    mp[make_pair(1, 2)].emplace_back(1);
-    mp[make_pair(1, 2)].emplace_back(2);
-    mp[make_pair(1, 2)].emplace_back(3);
-    mp[make_pair(1, 2)].emplace_back(4);
-    pair<vector<int>, vector<int>> pm;
-    vector<int> tmp{1, 2, 3};
-    vector<int> tmp2{4, 5, 6};
-    pm = make_pair(tmp, tmp2);
-    dbg(mp);
-    dbg(pm);
 }
