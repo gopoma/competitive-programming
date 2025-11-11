d6a141ab54393bc3ffbba30596f2949c3dc250b6
gopoma <gordono@unsa.edu.pe>
2025-11-10 22:54:56 -0500

    CF

--- tem.cpp
+++ tem.cpp
@@ -65,6 +65,8 @@ string to_string(A v) {
     return res;
 }
 
+
+
 void debug_out() { cerr << endl; }
 
 template <typename Head, typename... Tail>
@@ -73,18 +75,27 @@ void debug_out(Head H, Tail... T) {
   debug_out(T...);
 }
 
+
+
 #ifdef LOCAL
 const vector<string> __C_DBG{
     "\033[1;34m",
     "\033[0;32m = \033[35m",
-    "\033[0m", "]\033[35m:",
-    "\033[101m", "\033[0m"
+    "\033[0m",
+    "]\033[35m:",
+    "\033[101m",
+    "\033[0m"
 };
+
 #define MACRO(code) do {code} while (false)
+
 #define dbg(x)      MACRO(  \
     auto xd = x;    \
     cerr << "Line(" << __LINE__ << "): " << __C_DBG[0] << #x << __C_DBG[1] << to_string(xd) << __C_DBG[2] << endl;  \
 )
+
+
+
 #define dbg(...)    MACRO(  \
     cerr << "Line(" << __LINE__ << "): " << __C_DBG[0] << "[" << #__VA_ARGS__ << __C_DBG[3];    \
     debug_out(__VA_ARGS__);     \
@@ -104,7 +115,6 @@ const bool isDebugging = true;
 
 const bool isDebugging = false;
 #endif
-
 //* /Debugger
 
 
@@ -123,23 +133,35 @@ using ull  = unsigned long long;
 //? using u128 = __uint128_t;
 //? using f128 = __float128;
 
-using pi = pair<int,int>;
+
+
+using pi = pair<int, int>;
 using pl = pair<ll, ll>;
+using pd = pair<db, db>;
 #define mp make_pair
-#define f first
-#define s second
+#define f  first
+#define s  second
+
+
 
 #define tcT template<class T
+#define tcTU tcT, class U
+
 tcT> using V = vector<T>;
 tcT, size_t SZ> using AR = array<T,SZ>;
 using vi = V<int>;
-using vl = V<ll>;
 using vb = V<bool>;
+using vl = V<ll>;
+using vd = V<db>;
+using vs = V<str>;
 using vpi = V<pi>;
 using vpl = V<pl>;
+using vpd = V<pd>;
 
 #define sz(x) int((x).size())
-#define all(x) begin(x), end(x)
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
 #define sor(x) sort(all(x))
 #define rsz resize
 #define ins insert
@@ -151,21 +173,40 @@ using vpl = V<pl>;
 
 #define lb lower_bound
 #define ub upper_bound
+tcT > int lwb(V<T> &a, const T &b) { return int(lb(all(a), b) - bg(a)); }
+tcT > int upb(V<T> &a, const T &b) { return int(ub(all(a), b) - bg(a)); }
 
-#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
-#define F0R(i,a) FOR(i,0,a)
-#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
-#define R0F(i,a) ROF(i,0,a)
-#define rep(a) F0R(_,a)
-#define each(a,x) for (auto& a: x)
 
-const int MOD = 1e9+7;
+
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+
+
+const int MOD = int(1e9) + 7;
 const int INF = int(1e9) + 5;
 const ll BIG = ll(1e18) + 5;
 const db PI = acos((db)-1);
 const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};  //? for every grid problem!!
 mt19937 rng(0); // or mt19937_64
 //* mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+
+
+// bitwise ops
+// also see https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+constexpr int pct(int x) { return __builtin_popcount(x); }  // # of bits set
+constexpr int bits(int x) {  // assert(x >= 0); // make C++11 compatible until
+	                         // USACO updates ...
+	return x == 0 ? 0 : 31 - __builtin_clz(x);
+}  // floor(log2(x))
+constexpr int p2(int x) { return 1 << x; }
+constexpr int msk2(int x) { return p2(x) - 1; }
 
 ll cdiv(ll a, ll b) {
 	return a / b + ((a ^ b) > 0 && a % b);
@@ -174,18 +215,45 @@ ll fdiv(ll a, ll b) {
 	return a / b - ((a ^ b) < 0 && a % b);
 }  // divide a by b rounded down
 
-tcT> bool ckmin(T& a, const T& b) {
-	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
-tcT> bool ckmax(T& a, const T& b) {
-	return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+tcT > bool ckmin(T &a, const T &b) {
+	return b < a ? a = b, 1 : 0;
+}  // set a = min(a,b)
+tcT > bool ckmax(T &a, const T &b) {
+	return a < b ? a = b, 1 : 0;
+}  // set a = max(a,b)
 
+tcTU > T fstTrue(T lo, T hi, U f) {
+	++hi;
+	assert(lo <= hi);  // assuming f is increasing
+	while (lo < hi) {  // find first index such that f is true
+		T mid = lo + (hi - lo) / 2;
+		f(mid) ? hi = mid : lo = mid + 1;
+	}
+	return lo;
+}
+tcTU > T lstTrue(T lo, T hi, U f) {
+	--lo;
+	assert(lo <= hi);  // assuming f is decreasing
+	while (lo < hi) {  // find first index such that f is true
+		T mid = lo + (hi - lo + 1) / 2;
+		f(mid) ? lo = mid : hi = mid - 1;
+	}
+	return lo;
+}
 tcT > void remDup(vector<T> &v) {  // sort and remove duplicates
 	sort(all(v));
 	v.erase(unique(all(v)), end(v));
 }
+tcTU > void safeErase(T &t, const U &u) {
+	auto it = t.find(u);
+	assert(it != end(t));
+	t.erase(it);
+}
+
+
 
 void setIn(string s) { freopen(s.c_str(), "r", stdin); }
-void setOut(str s) { freopen(s.c_str(), "w", stdout); }
+void setOut(string s) { freopen(s.c_str(), "w", stdout); }
 
 const auto beg_time = std::chrono::high_resolution_clock::now();
 double time_elapsed() {
@@ -194,6 +262,9 @@ double time_elapsed() {
 	    .count();
 }
 
+
+
+//? Custom Helpers
 template <typename T>
 inline T gcd(T a, T b) { while (b != 0) swap(b, a %= b); return a; }
 
@@ -201,7 +272,7 @@ long long binpow(long long a, long long b) {
     long long res = 1;
     while (b > 0) {
         if (b & 1)
-        res = res * a;
+            res = res * a;
         a = a * a;
         b >>= 1;
     }
@@ -214,6 +285,7 @@ const int dddy[8]{0, 1,  0, -1, 1, -1,  1, -1};
 using vvi = V<vi>;
 using vvl = V<vl>;
 using vvb = V<vb>;
+//? /Custom Helpers
 
 
 
@@ -221,6 +293,9 @@ using vvb = V<vb>;
 //* /Template
 
 void solve() {
+    // run A < A3.in
+    // xd A < A4.in
+
 
 }
 
