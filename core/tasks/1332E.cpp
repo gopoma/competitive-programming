c6f44a44487d84da5bdde3b73bb7cbcd90219e8f
gopoma <gordono@unsa.edu.pe>
2026-07-30 01:20:12 -0500

    CF

---
+++ core/tasks/1332E.cpp
@@ -0,0 +1,539 @@
+//* sometimes pragmas don't work, if so, just comment it!
+// #pragma GCC optimize ("Ofast")
+//? #pragma GCC target ("avx,avx2")
+#pragma GCC optimize ("trapv")
+
+//! #undef _GLIBCXX_DEBUG //? for Stress Testing
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+//* Debugger
+string to_string(string s) {
+    return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+    return to_string((string) s);
+}
+
+string to_string(char c) {
+    return string(1, c);
+}
+
+string to_string(bool b) {
+    return (b ? "true" : "false");
+}
+
+template <typename A>
+string to_string(A v);
+
+string to_string(vector<bool> v) {
+    bool first = true;
+    string res = "[";
+    for (auto x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += x ? "true" : "false";
+    }
+    res += "]";
+    return res;
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "[";
+    for (const auto &x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "]";
+    return res;
+}
+
+
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+
+
+
+#ifdef LOCAL
+const vector<string> __C_DBG{
+    "\033[1;34m",
+    "\033[0;32m = \033[35m",
+    "\033[0m",
+    "]\033[35m:",
+    "\033[101m",
+    "\033[0m"
+};
+
+#define MACRO(code) do {code} while (false)
+
+#define dbg(x)      MACRO(  \
+    auto xd = x;    \
+    cerr << "Line(" << __LINE__ << "): " << __C_DBG[0] << #x << __C_DBG[1] << to_string(xd) << __C_DBG[2] << endl;  \
+)
+
+
+
+#define dbg(...)    MACRO(  \
+    cerr << "Line(" << __LINE__ << "): " << __C_DBG[0] << "[" << #__VA_ARGS__ << __C_DBG[3];    \
+    debug_out(__VA_ARGS__);     \
+    cerr << __C_DBG[2];     \
+)
+
+#define GA          dbg(0)
+// RAYA tiene 32 caracteres "="
+#define RAYA        cerr << __C_DBG[4] << "================================" << __C_DBG[5] << endl;
+
+const bool isDebugging = true;
+#else
+#define dbg(x)
+#define dbg(...)
+#define GA
+#define RAYA
+
+const bool isDebugging = false;
+#endif
+//* /Debugger
+
+
+
+using ll = long long;
+using db = long double; // or double if tight TL
+using str = string;
+
+//? priority_queue for minimum
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+using ull  = unsigned long long;
+//? using i64  = long long;
+//? using u64  = uint64_t;
+//? using i128 = __int128;
+//? using u128 = __uint128_t;
+//? using f128 = __float128;
+
+
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+#define mp make_pair
+#define f  first
+#define s  second
+
+
+
+#define tcT template<class T
+#define tcTU tcT, class U
+
+tcT> using V = vector<T>;
+tcT, size_t SZ> using AR = array<T,SZ>;
+using vi = V<int>;
+using vb = V<bool>;
+using vl = V<ll>;
+using vd = V<db>;
+using vs = V<str>;
+using vpi = V<pi>;
+using vpl = V<pl>;
+using vpd = V<pd>;
+
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+#define ts to_string
+
+#define lb lower_bound
+#define ub upper_bound
+tcT > int lwb(V<T> &a, const T &b) { return int(lb(all(a), b) - bg(a)); }
+tcT > int upb(V<T> &a, const T &b) { return int(ub(all(a), b) - bg(a)); }
+
+
+
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+
+
+const int MOD = 998244353;
+const int INF = int(1e9) + 5;
+const ll BIG = ll(1e18) + 5;
+const db PI = acos((db)-1);
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};  //? for every grid problem!!
+mt19937 rng(0); // or mt19937_64
+//* mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+
+
+// bitwise ops
+// also see https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+constexpr int pct(int x) { return __builtin_popcount(x); }  // # of bits set
+constexpr int bits(int x) {  // assert(x >= 0); // make C++11 compatible until
+	                         // USACO updates ...
+	return x == 0 ? 0 : 31 - __builtin_clz(x);
+}  // floor(log2(x))
+constexpr int p2(int x) { return 1 << x; }
+constexpr int msk2(int x) { return p2(x) - 1; }
+
+ll cdiv(ll a, ll b) {
+	return a / b + ((a ^ b) > 0 && a % b);
+}  // divide a by b rounded up
+ll fdiv(ll a, ll b) {
+	return a / b - ((a ^ b) < 0 && a % b);
+}  // divide a by b rounded down
+
+tcT > bool ckmin(T &a, const T &b) {
+	return b < a ? a = b, 1 : 0;
+}  // set a = min(a,b)
+tcT > bool ckmax(T &a, const T &b) {
+	return a < b ? a = b, 1 : 0;
+}  // set a = max(a,b)
+
+tcTU > T fstTrue(T lo, T hi, U f) {
+	++hi;
+	assert(lo <= hi);  // assuming f is increasing
+	while (lo < hi) {  // find first index such that f is true
+		T mid = lo + (hi - lo) / 2;
+		f(mid) ? hi = mid : lo = mid + 1;
+	}
+	return lo;
+}
+tcTU > T lstTrue(T lo, T hi, U f) {
+	--lo;
+	assert(lo <= hi);  // assuming f is decreasing
+	while (lo < hi) {  // find first index such that f is true
+		T mid = lo + (hi - lo + 1) / 2;
+		f(mid) ? lo = mid : hi = mid - 1;
+	}
+	return lo;
+}
+tcT > void remDup(vector<T> &v) {  // sort and remove duplicates
+	sort(all(v));
+	v.erase(unique(all(v)), end(v));
+}
+tcTU > void safeErase(T &t, const U &u) {
+	auto it = t.find(u);
+	assert(it != end(t));
+	t.erase(it);
+}
+
+
+
+void setIn(string s) { freopen(s.c_str(), "r", stdin); }
+void setOut(string s) { freopen(s.c_str(), "w", stdout); }
+
+const auto beg_time = std::chrono::high_resolution_clock::now();
+double time_elapsed() {
+	return chrono::duration<double>(std::chrono::high_resolution_clock::now() -
+	                                beg_time)
+	    .count();
+}
+
+
+
+//? Custom Helpers
+template <typename T>
+inline T gcd(T a, T b) { while (b != 0) swap(b, a %= b); return a; }
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+const int dddx[8]{1, 0, -1,  0, 1,  1, -1, -1};
+const int dddy[8]{0, 1,  0, -1, 1, -1,  1, -1};
+
+using vvi = V<vi>;
+using vvl = V<vl>;
+using vvb = V<vb>;
+
+ll custom_abs(ll x) {
+    if(x < 0) return -x;
+    return +x;
+}
+//? /Custom Helpers
+
+
+
+//* Template
+
+/**
+ * Description: modular arithmetic operations
+ * Source:
+ * KACTL
+ * https://codeforces.com/blog/entry/63903
+ * https://codeforces.com/contest/1261/submission/65632855 (tourist)
+ * https://codeforces.com/contest/1264/submission/66344993 (ksun)
+ * also see https://github.com/ecnerwala/cp-book/blob/master/src/modnum.hpp
+ * (ecnerwal) Verification: https://open.kattis.com/problems/modulararithmetic
+ */
+
+template <int MOD, int RT> struct mint {
+	static const int mod = MOD;
+	static constexpr mint rt() { return RT; }  // primitive root for FFT
+	int v;
+	explicit operator int() const {
+		return v;
+	}  // explicit -> don't silently convert to int
+	mint() : v(0) {}
+	mint(ll _v) {
+		v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
+		if (v < 0) v += MOD;
+	}
+	bool operator==(const mint &o) const { return v == o.v; }
+	friend bool operator!=(const mint &a, const mint &b) { return !(a == b); }
+	friend bool operator<(const mint &a, const mint &b) { return a.v < b.v; }
+	friend istream &operator>>(istream &is, mint &a) {
+		ll x;
+		is >> x;
+		a = mint(x);
+		return is;
+	}
+	friend ostream &operator<<(ostream &os, mint a) {
+		os << int(a);
+		return os;
+	}
+
+	mint &operator+=(const mint &o) {
+		if ((v += o.v) >= MOD) v -= MOD;
+		return *this;
+	}
+	mint &operator-=(const mint &o) {
+		if ((v -= o.v) < 0) v += MOD;
+		return *this;
+	}
+	mint &operator*=(const mint &o) {
+		v = int((ll)v * o.v % MOD);
+		return *this;
+	}
+	mint &operator/=(const mint &o) { return (*this) *= inv(o); }
+	friend mint pow(mint a, ll p) {
+		mint ans = 1;
+		assert(p >= 0);
+		for (; p; p /= 2, a *= a)
+			if (p & 1) ans *= a;
+		return ans;
+	}
+	friend mint inv(const mint &a) {
+		assert(a.v != 0);
+		return pow(a, MOD - 2);
+	}
+
+	mint operator-() const { return mint(-v); }
+	mint &operator++() { return *this += 1; }
+	mint &operator--() { return *this -= 1; }
+	friend mint operator+(mint a, const mint &b) { return a += b; }
+	friend mint operator-(mint a, const mint &b) { return a -= b; }
+	friend mint operator*(mint a, const mint &b) { return a *= b; }
+	friend mint operator/(mint a, const mint &b) { return a /= b; }
+};
+
+using mi = mint<MOD, 5>;  // 5 is primitive root for both common mods
+using vmi = V<mi>;
+using pmi = pair<mi, mi>;
+using vpmi = V<pmi>;
+
+//* /Template
+
+/**
+ * Description: Combinations modulo a prime $MOD$. Assumes $2\le N \le MOD$.
+ * Time: O(N)
+ * Source: KACTL
+ * Verification: https://dmoj.ca/problem/tle17c4p5
+ * Usage: F.init(10); F.C(6, 4); // 15
+ */
+
+struct {
+	vmi invs, fac, ifac;
+	void init(int N) { // idempotent
+		invs.rsz(N), fac.rsz(N), ifac.rsz(N);
+		invs[1] = fac[0] = ifac[0] = 1;
+		FOR(i,2,N) invs[i] = mi(-(ll)MOD/i*(int)invs[MOD%i]);
+		FOR(i,1,N) fac[i] = fac[i-1]*i, ifac[i] = ifac[i-1]*invs[i];
+	}
+	mi C(int a, int b) {
+		if (a < b || b < 0) return 0;
+		return fac[a]*ifac[b]*ifac[a-b];
+	}
+} F;
+
+
+mi brute(ll n, ll m, ll L, ll R) {
+    auto work = [&](ll nm, ll e, ll o) -> mi {
+        mi re = 0;
+        for(ll cnt = 0; cnt <= nm; cnt += 2) {
+            assert(nm >= cnt);
+            re += F.C(nm, cnt) * pow(mi(o), cnt) * pow(mi(e), nm - cnt);
+        }
+        return re;
+    };
+
+
+    ll T = n * m;
+    ll count = R - L + 1;
+
+    ll odds = 0, evens = 0;
+    if(L & 1) {
+        odds = (count + 1) / 2;
+        evens = count / 2;
+    } else {
+        odds = count / 2;
+        evens = (count + 1) / 2;
+    }
+
+    return work(T, evens, odds);
+}
+
+void slv(ll n, ll m, ll L, ll R) {
+    const ll T = n * m;
+    const ll count = R - L + 1;
+    dbg(T, count);
+
+    mi response = 0;
+
+    if(count % 2 == 0) {
+        response = pow(mi(count / 2), T) * pow(mi(2), T - 1);
+    } else {
+        ll oo = (count - 1) / 2;
+        if(T % 2 == 0) {
+            response = (pow(mi(2 * oo) + 1, T) + 1) / 2;
+        } else {
+            mi delta = 0;
+            if(oo & 1) delta = -1;
+            else delta = +1;
+
+            response = (pow(mi(2 * oo) + 1, T) + delta) / 2;
+            dbg(count, oo, int(delta), int(response));
+
+            if((T & 1) && (oo & 1)) {
+                response = response + 1;
+            }
+        }
+
+        if((L & 1) && (T & 1)) {
+            response = response - 1;
+        }
+    }
+
+    if(T & 1) {
+        response = pow(mi(count), T);
+    }
+
+    dbg(int(response));
+    cout << int(response) << "\n";
+
+    if(isDebugging && 1) {
+        dbg("Brute");
+        mi ans = brute(n, m, L, R);
+        dbg("/Brute");
+        dbg(int(response), int(ans));
+        assert(response == ans);
+    }
+}
+
+void solve() {
+    ll n, m, L, R; cin >> n >> m >> L >> R;
+    dbg(n, m, L, R);
+
+    slv(n, m, L, R);
+}
+
+
+// return int in [L,R] inclusive
+int rng_int(int L, int R) { assert(L <= R);
+	return uniform_int_distribution<int>(L,R)(rng);
+}
+
+ll rng_ll(ll L, ll R) { assert(L <= R);
+	return uniform_int_distribution<ll>(L,R)(rng);
+}
+
+// return double in [L,R] inclusive
+db rng_db(db L, db R) { assert(L <= R);
+	return uniform_real_distribution<db>(L,R)(rng);
+}
+
+template<class T> void shuf(vector<T>& v) { shuffle(all(v),rng); }
+
+// generate edges of tree with verts [0,N-1]
+// smaller back -> taller tree
+vpi treeRand(int N, int back) {
+	assert(N >= 1 && back >= 0); vpi ed;
+	FOR(i,1,N) ed.eb(i,i-1-rng_int(0,min(back,i-1)));
+	return ed;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    if(isDebugging) {
+        F.init(int(1e6));
+    }
+
+    while(isDebugging && 1) {
+        RAYA;
+        RAYA;
+        RAYA;
+        ll n = rng_ll(1, 1e1);
+        ll m = rng_ll(1, 1e1);
+        ll L = rng_ll(1, 1e1);
+        ll R = rng_ll(L, 1e1);
+        dbg(n, m, L, R);
+        slv(n, m, L, R);
+    }
+
+    int t = 1;
+    while(t--) {
+        RAYA;
+        RAYA;
+        RAYA;
+        solve();
+    }
+
+
+    #ifdef LOCAL
+        cerr << fixed << setprecision(5);
+        cerr << "\033[42m++++++++++++++++++++\033[0m\n";
+        cerr << "\033[42mtime = " << time_elapsed() << "ms\033[0m\n";
+        cerr << "\033[42m++++++++++++++++++++\033[0m";
+    #endif
+}
